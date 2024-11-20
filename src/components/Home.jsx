7f85ac20b6a75099c943a141f10822253f2469fc
dmryrkhtc <demiryurekhatice@outlook.com>
2024-11-20 15:38:17 +0300

    adding Home.css and Home.jsx,fixing App.js

---
+++ src/components/Home.jsx
@@ -0,0 +1,30 @@
+// Home.jsx
+import React from 'react';
+import './Home.css'; // CSS dosyasını import ediyoruz
+
+const Home = () => {
+    return (
+        <div className="home-container">
+            <header className="home-header">
+                <h1>Akıllı Park Sistemi</h1>
+                <p>Bu proje, park alanlarını daha verimli yönetmeyi amaçlayan bir akıllı park sistemini geliştirmeyi hedefliyor.</p>
+            </header>
+
+            <section className="team-section">
+                <h2>Ekip Üyeleri</h2>
+
+                <li> Hatice Demiryürek</li>
+                <li>Sıla Kasalı</li>
+                <li>Furkan Öger</li><li>Melike Akyol</li><li>Beyza Busenur Çelik</li><li>Esra Turan</li><li>Elif Dağlar</li><li>Muhammed Emin Doğan</li><li>Emir Karakaş</li>
+
+            </section>
+
+            <section className="goal-section">
+                <h2>Proje Amacı</h2>
+                <p>Projemiz, sensörler aracılığıyla sesli ve görsel geri bildirimler vererek arabayı kolay park etmemize yardımcı oluyor.</p>
+            </section>
+        </div>
+    );
+};
+
+export default Home;
